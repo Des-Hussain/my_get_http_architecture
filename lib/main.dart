6e4858a8ce3a5f5c8ceba97950e01091e8b30b61
Hussain <mohammadhussainmobeen@gmail.com>
2024-06-13 21:35:10 +0500

    case tracker

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,9 @@ void main() {
   LocalStorageService.instance.init();
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MyApp());
+  runApp(const CaseTracker());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const CaseTracker(),
-    );
-  }
-}
 
 class MyHttpOverrides extends HttpOverrides {
   @override
